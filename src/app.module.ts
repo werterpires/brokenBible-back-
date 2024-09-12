327e578bf7bbac91a764bad3d785f95abce2a460
Werter Pires <werterpires23@hotmail.com>
2024-09-12 06:09:05 -0300

    configuring eslint and prettier

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,50 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { KnexModule, KnexModuleOptions } from 'nest-knexjs'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+export const mysqlConfig: KnexModuleOptions = {
+  config: {
+    client: 'mysql2',
+    useNullAsDefault: true,
+    connection: {
+      host: process.env.SQL_DEV_HOST,
+      user: process.env.SQL_DEV_USER,
+      password: process.env.SQL_DEV_PASS,
+      database: process.env.SQL_DEV_DB,
+      ssl: undefined,
+      typeCast: function (field, next) {
+        if (field.type === 'TINY' && field.length === 1) {
+          // retorna tipo booleano ou null
+          switch (field.string()) {
+            case null:
+            case undefined:
+            case '':
+            case 'null':
+            case 'NULL':
+              return null
+            case '0':
+              return false
+            case '1':
+              return true
+          }
+        } else if (field.type === 'DATE' && field.length > 1) {
+          return field.string()
+        } else if (field.type === 'DATETIME' && field.length > 1) {
+          return field.string().substring(0, 10)
+        }
+        return next()
+      }
+    }
+  }
+}
 
 @Module({
-  imports: [],
+  imports: [KnexModule.forRoot(mysqlConfig)],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
